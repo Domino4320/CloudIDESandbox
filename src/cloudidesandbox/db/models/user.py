f7d1368a0cad4101a17e9abd780f5a7767e667ea
Ruslan <pprrg18@gmail.com>
2026-08-08 14:19:16 +0300

    feat(db): add users table migration

--- src/cloudidesandbox/db/models/user.py
+++ src/cloudidesandbox/db/models/user.py
@@ -3,6 +3,6 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 
 class User(Base):
-
+    __tablename__ = "users"
     login: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str] = mapped_column()
