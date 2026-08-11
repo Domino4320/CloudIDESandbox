0ba195417954a63d6e42a7042d179c7348e89a3d
Ruslan <pprrg18@gmail.com>
2026-08-11 20:37:12 +0300

    refactor(terminal service): change service work logic

--- src/cloudidesandbox/services/terminal.py
+++ src/cloudidesandbox/services/terminal.py
@@ -1,64 +1,55 @@
-from __future__ import annotations
 import asyncio
-from asyncio.subprocess import Process
 import os
-from typing import TYPE_CHECKING
+from typing import Callable, Awaitable
 
-if TYPE_CHECKING:
-    from fastapi import WebSocket
+OutputCallback = Callable[[str], Awaitable[None]]
 
 
 class TerminalService:
 
-    def __init__(self, websocket: WebSocket, container_id: str):
-        self.websocket = websocket
+    def __init__(self, container_id: str, output_callback: OutputCallback):
+        self.output_callback = output_callback
         self.container_id = container_id
+        self._master_fd = None
+        self._proc = None
 
-    async def _start_process(self) -> tuple[int, Process]:
-        master_fd, slave_fd = os.openpty()
+    async def start(self) -> None:
+        self._master_fd, slave_fd = os.openpty()
         try:
-            proc = await asyncio.create_subprocess_exec(
+            self._proc = await asyncio.create_subprocess_exec(
                 "docker",
                 "exec",
                 "-it",
                 self.container_id,
                 "/bin/bash",
-                stdin=slave_fd,
-                stdout=slave_fd,
+                stdin=self._slave_fd,
+                stdout=self._slave_fd,
                 stderr=slave_fd,
                 preexec_fn=os.setsid,
             )
         finally:
             os.close(slave_fd)
-        return master_fd, proc
 
-    async def _pty_to_web(self, master_fd: int) -> None:
+    async def read_loop(self) -> None:
         while True:
-            raw_response = await asyncio.to_thread(os.read, master_fd, 1024)
+            raw_response = await asyncio.to_thread(os.read, self._master_fd, 1024)
             response = raw_response.decode(errors="ignore")
             if not response:
-                return
-            await self.websocket.send_text(response)
+                break
+            await self.output_callback(response)
 
-    async def _web_to_pty(self, master_fd: int) -> None:
-        while True:
-            request = await self.websocket.receive_text()
-            if not request.endswith("\n"):
-                request += "\n"
-            await asyncio.to_thread(os.write, master_fd, request.encode("utf-8"))
+    async def write_input(self, data: str) -> None:
+        if not data.endswith("\n"):
+            data += "\n"
+        await asyncio.to_thread(os.write, self._master_fd, data.encode("utf-8"))
 
-    async def run(self) -> None:
-        try:
-            master_fd, proc = await self._start_process()
-            task_read = asyncio.create_task(self._pty_to_web(master_fd))
-            task_write = asyncio.create_task(self._web_to_pty(master_fd))
-            _, pending = await asyncio.wait(
-                [task_read, task_write],
-                return_when=asyncio.FIRST_COMPLETED,
-            )
-        finally:
-            for task in pending:
-                task.cancel()
-            os.close(master_fd)
-            proc.terminate()
-            await proc.wait()
+    async def close(self):
+        if self._master_fd:
+            os.close(self._master_fd)
+            self._master_fd = None
+
+        if self._proc:
+            if self._proc.returncode is None:
+                self._proc.terminate()
+                await self._proc.wait()
+            self._proc = None
