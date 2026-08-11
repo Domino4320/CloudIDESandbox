5c1e525ecab6a1f30063be498ab67f2f924b38be
Ruslan <pprrg18@gmail.com>
2026-08-11 14:35:48 +0300

    fix(exceptions): change structure under base class for container not found exception

--- src/cloudidesandbox/exceptions/containers.py
+++ src/cloudidesandbox/exceptions/containers.py
@@ -1,7 +1,10 @@
-class ContainerError(Exception):
-    pass
+from src.cloudidesandbox.exceptions.base import AppException
 
 
-class ContainerNotFoundError(ContainerError):
+class ContainerNotFoundError(AppException):
+
+    status_code = 404
+    code = "CONTAINER_NOT_FOUND"
+
     def __init__(self, container_id):
-        super().__init__(f"Container {container_id} was not found")
+        super().__init__(detail=f"Container {container_id} was not found")
