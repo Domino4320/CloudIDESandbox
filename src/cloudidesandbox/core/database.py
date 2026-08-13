e3bd49d6badd14a7959e0ac6eeb2a5ad617a359c
Ruslan <pprrg18@gmail.com>
2026-08-13 19:46:21 +0300

    fix(database): add session auto-commiting into generator

--- src/cloudidesandbox/core/database.py
+++ src/cloudidesandbox/core/database.py
@@ -7,4 +7,5 @@ sessionmaker = async_sessionmaker(engine, expire_on_commit=False)
 
 async def get_session():
     async with sessionmaker() as session:
-        yield session
+        async with session.begin():
+            yield session
